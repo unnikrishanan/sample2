3d28200e712d5d399941497ca91f1457d5213068
Vishnu KR <vishnukr1101@gmail.com>
2022-07-12 23:20:34 +0530

    Initial commit

---
+++ bin/sample2.dart
@@ -0,0 +1,8 @@
+import 'package:sample2/sample2.dart' as sample2;
+
+void main(List<String> arguments) {
+  var name="unni";
+  var age= 23;
+  var email="unniedaayaranz004";
+  print("my name is $name");
+}
---
+++ test/sample2_test.dart
@@ -0,0 +1,8 @@
+import 'package:sample2/sample2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
